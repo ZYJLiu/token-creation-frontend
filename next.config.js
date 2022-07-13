410cf9f62284a9865f2470350bd727911bed09eb
john <johnzhengyuliu@gmail.com>
2022-07-13 04:26:16 -0500

    vercel

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
 module.exports = {
-  nextConfig,
-  webpack(config){
-    config.resolve.fallback = { 
-      fs: false, 
-      path: false, 
-      constants: false, 
-      stream: require.resolve('stream-browserify'), 
-      crypto: require.resolve('crypto-browserify'),
-      zlib: require.resolve('browserify-zlib')
-    }
-    return config;
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
