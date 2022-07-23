d82820597b6a959ffab29626ecec3ef5550fef6e
john <johnzhengyuliu@gmail.com>
2022-07-22 19:45:16 -0500

    vercel

--- src/pages/merchants/[PK].tsx
+++ src/pages/merchants/[PK].tsx
@@ -11,7 +11,9 @@ export default function Promo() {
     const { PK } = router.query
     const workspace = useWorkspace()
 
-    const merchant = new PublicKey(PK) || null
+    const merchant =
+        new PublicKey(PK) ||
+        new PublicKey("HexFnfwS4Rp8abu2Y4EnT44NeQf7KFdVdEhYNV2EPvbs") // placeholder for vercel
 
     const [accounts, setAccounts] = useState(null)
 
