35239493489f3899870cc042157af4cb27eacccd
Griffin McLennan <mclennangriffin@gmail.com>
2022-07-25 03:11:37 -0700

    fix: adjust css

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,15 +2,15 @@ import { useEffect, useMemo, useRef } from "react"
 import styles from "../styles/custom.module.css"
 
 import {
-    createQR,
-    createQROptions,
-    encodeURL,
-    TransferRequestURLFields,
-    findReference,
-    validateTransfer,
-    FindReferenceError,
-    ValidateTransferError,
-    TransactionRequestURLFields,
+  createQR,
+  createQROptions,
+  encodeURL,
+  TransferRequestURLFields,
+  findReference,
+  validateTransfer,
+  FindReferenceError,
+  ValidateTransferError,
+  TransactionRequestURLFields,
 } from "@solana/pay"
 
 import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js"
@@ -27,138 +27,131 @@ import { useRouter } from "next/router"
 import Confirmed from "../../src/components/Confirmed"
 
 const MODAL_STYLES = {
-    position: "fixed",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    backgroundColor: "#FFF",
-    padding: "10px",
-    zIndex: 1000,
+  position: "fixed",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  backgroundColor: "#FFF",
+  padding: "10px",
+  zIndex: 1000,
+  display: "flex",
+  flexDirection: "column",
 }
 
 const OVERLAY_STYLES = {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: 1000,
-    backgroundColor: "rgba(0, 0, 0, 0.3)",
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: 1000,
+  backgroundColor: "rgba(0, 0, 0, 0.3)",
 }
 
 // modal to display attributes
 export default function Modal({ open, data, onClose }) {
-    if (!open) return null
+  if (!open) return null
 
-    const router = useRouter()
-    const workspace = useWorkspace()
+  const router = useRouter()
+  const workspace = useWorkspace()
 
-    // Get a connection to Solana devnet
-    const network = WalletAdapterNetwork.Devnet
-    const endpoint = clusterApiUrl(network)
-    const connection = workspace.connection
+  // Get a connection to Solana devnet
+  const network = WalletAdapterNetwork.Devnet
+  const endpoint = clusterApiUrl(network)
+  const connection = workspace.connection
 
-    const [reference, setReference] = useState(Keypair.generate().publicKey)
+  const [reference, setReference] = useState(Keypair.generate().publicKey)
 
-    const { publicKey } = useWallet()
+  const { publicKey } = useWallet()
 
-    const [confirmed, setConfirmed] = useState(false)
+  const [confirmed, setConfirmed] = useState(false)
 
-    //QR code
-    const searchParams = new URLSearchParams()
+  //QR code
+  const searchParams = new URLSearchParams()
 
-    // const reference = useMemo(() => Keypair.generate().publicKey, [])
-    searchParams.append("reference", reference.toString())
-    searchParams.append("promo", data.toString())
+  // const reference = useMemo(() => Keypair.generate().publicKey, [])
+  searchParams.append("reference", reference.toString())
+  searchParams.append("promo", data.toString())
 
-    const [size, setSize] = useState(() =>
-        typeof window === "undefined"
-            ? 400
-            : Math.min(window.screen.availWidth - 10, 512)
-    )
+  const [size, setSize] = useState(() =>
+    typeof window === "undefined" ? 400 : Math.min(window.screen.availWidth - 10, 512)
+  )
 
-    useEffect(() => {
-        const listener = () =>
-            setSize(Math.min(window.screen.availWidth - 10, 512))
+  useEffect(() => {
+    const listener = () => setSize(Math.min(window.screen.availWidth - 10, 512))
 
-        window.addEventListener("resize", listener)
-        return () => window.removeEventListener("resize", listener)
-    }, [])
+    window.addEventListener("resize", listener)
+    return () => window.removeEventListener("resize", listener)
+  }, [])
 
-    const qrRef = useRef<HTMLDivElement>(null)
+  const qrRef = useRef<HTMLDivElement>(null)
 
-    useEffect(() => {
-        const { location } = window
-        const apiUrl = `${location.protocol}//${
-            location.host
-        }/api/makeTransactionNft?${searchParams.toString()}`
-        const urlParams: TransactionRequestURLFields = {
-            link: new URL(apiUrl), // testing placeholder
-            label: "Test",
-            message: "Test Message",
+  useEffect(() => {
+    const { location } = window
+    const apiUrl = `${location.protocol}//${location.host}/api/makeTransactionNft?${searchParams.toString()}`
+    const urlParams: TransactionRequestURLFields = {
+      link: new URL(apiUrl), // testing placeholder
+      label: "Test",
+      message: "Test Message",
+    }
+    const solanaUrl = encodeURL(urlParams)
+    const qr = createQR(solanaUrl, size, "white")
+
+    if (qrRef.current) {
+      qrRef.current.innerHTML = ""
+      qr.append(qrRef.current)
+    }
+  }, [data])
+
+  // Check every 0.5s if the transaction is completed
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        // Check if there is any transaction for the reference
+        const signatureInfo = await findReference(connection, reference, {
+          finality: "confirmed",
+        })
+        setConfirmed(true)
+        // router.push("/confirmedNft")
+        // console.log("confirmed");
+      } catch (e) {
+        if (e instanceof FindReferenceError) {
+          // No transaction found yet, ignore this error
+          return
         }
-        const solanaUrl = encodeURL(urlParams)
-        const qr = createQR(solanaUrl, size, "white")
-
-        if (qrRef.current) {
-            qrRef.current.innerHTML = ""
-            qr.append(qrRef.current)
+        if (e instanceof ValidateTransferError) {
+          // Transaction is invalid
+          console.error("Transaction is invalid", e)
+          return
         }
-    }, [data])
+        console.error("Unknown error", e)
+      }
+    }, 500)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [reference.toString()])
 
-    // Check every 0.5s if the transaction is completed
-    useEffect(() => {
-        const interval = setInterval(async () => {
-            try {
-                // Check if there is any transaction for the reference
-                const signatureInfo = await findReference(
-                    connection,
-                    reference,
-                    {
-                        finality: "confirmed",
-                    }
-                )
-                setConfirmed(true)
-                // router.push("/confirmedNft")
-                // console.log("confirmed");
-            } catch (e) {
-                if (e instanceof FindReferenceError) {
-                    // No transaction found yet, ignore this error
-                    return
-                }
-                if (e instanceof ValidateTransferError) {
-                    // Transaction is invalid
-                    console.error("Transaction is invalid", e)
-                    return
-                }
-                console.error("Unknown error", e)
-            }
-        }, 500)
-        return () => {
-            clearInterval(interval)
-        }
-    }, [reference.toString()])
-
-    return (
-        <div>
-            <div style={OVERLAY_STYLES} />
-            <div style={MODAL_STYLES}>
-                {confirmed ? (
-                    <div style={{ width: size }}>
-                        <Confirmed />
-                    </div>
-                ) : (
-                    <div>
-                        <div ref={qrRef} />
-                    </div>
-                )}
-                <button
-                    className=" btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
-                    onClick={onClose}
-                >
-                    Close
-                </button>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div style={OVERLAY_STYLES} />
+      <div style={MODAL_STYLES}>
+        {confirmed ? (
+          <div style={{ width: size }}>
+            <Confirmed />
+          </div>
+        ) : (
+          <div>
+            <div ref={qrRef} />
+          </div>
+        )}
+        <button
+          className="w-15 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
+          onClick={onClose}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  )
 }
--- src/components/Promo.tsx
+++ src/components/Promo.tsx
@@ -4,60 +4,52 @@ import { useWorkspace } from "contexts/Workspace"
 import Modal from "../../src/components/Modal"
 
 export interface PromoProps {
-    account: string
+  account: string
 }
 
 export const DisplayPromo: FC<PromoProps> = (props) => {
-    const [data, setData] = useState(null)
-    const [modalData, setModalData] = useState(null)
-    const [isOpen, setIsOpen] = useState(false)
+  const [data, setData] = useState(null)
+  const [modalData, setModalData] = useState(null)
+  const [isOpen, setIsOpen] = useState(false)
 
-    const workspace = useWorkspace()
-    const connection = workspace.connection
-    const metaplex = new Metaplex(connection)
+  const workspace = useWorkspace()
+  const connection = workspace.connection
+  const metaplex = new Metaplex(connection)
 
-    const run = useRef(true)
-    useEffect(() => {
-        const fetchData = async () => {
-            const metadata = await metaplex
-                .nfts()
-                .findByMint(props.account.account.mint)
-            let fetchResult = await fetch(metadata.uri)
-            let json = await fetchResult.json()
-            console.log(json)
-            setData(json)
-        }
-        if (run.current) {
-            run.current = false
-            fetchData()
-        }
-    }, [props])
+  const run = useRef(true)
+  useEffect(() => {
+    const fetchData = async () => {
+      const metadata = await metaplex.nfts().findByMint(props.account.account.mint)
+      let fetchResult = await fetch(metadata.uri)
+      let json = await fetchResult.json()
+      console.log(json)
+      setData(json)
+    }
+    if (run.current) {
+      run.current = false
+      fetchData()
+    }
+  }, [props])
 
-    return (
-        <div>
-            {data && (
-                <div>
-                    <img src={data.image} />
-                    <ul>{data.name}</ul>
-                    <ul>{data.description}</ul>
-                    <button
-                        className="px-1 m-1 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-200 hover:to-yellow-500 ..."
-                        onClick={() => {
-                            setModalData(props.account.publicKey)
-                            setIsOpen(true)
-                        }}
-                    >
-                        Mint Promo
-                    </button>
-                    {isOpen && (
-                        <Modal
-                            data={modalData}
-                            open={isOpen}
-                            onClose={() => setIsOpen(false)}
-                        />
-                    )}
-                </div>
-            )}
+  return (
+    <div>
+      {data && (
+        <div className="flex flex-col items-center justify-center p-5">
+          <img src={data.image} />
+          <ul>{data.name}</ul>
+          <ul>{data.description}</ul>
+          <button
+            className="px-1 m-1 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-200 hover:to-yellow-500 ..."
+            onClick={() => {
+              setModalData(props.account.publicKey)
+              setIsOpen(true)
+            }}
+          >
+            Mint Promo
+          </button>
+          {isOpen && <Modal data={modalData} open={isOpen} onClose={() => setIsOpen(false)} />}
         </div>
-    )
+      )}
+    </div>
+  )
 }
